08225d80ba9141b2f4b6661dbb0ff7b14e1d2a2f
Daniel Quadros <80217777+danieltquadros@users.noreply.github.com>
2024-06-20 11:49:31 -0300

    [PORT-0008] - feat: Estruturado a WelcomePage e adicionado o nome.

--- components/WelcomePage/index.tsx
+++ components/WelcomePage/index.tsx
@@ -3,9 +3,28 @@ import React from 'react';
 
 // Global components
 import PageContainer from '../PageContainer';
+import {
+  LeftPanel,
+  RightPanel,
+  TitleContainer,
+  WelcomePageContainer,
+} from './styles';
 
 const WelcomePage = () => {
-  return <PageContainer id="welcome">WelcomePage</PageContainer>;
+  return (
+    <PageContainer id="welcome">
+      <WelcomePageContainer>
+        <LeftPanel>
+          <TitleContainer>
+            <span className="myName">Elisiane Quadros</span>
+            <br />
+            <span className="myPosition">Desenvolvedora Web</span>
+          </TitleContainer>
+        </LeftPanel>
+        <RightPanel />
+      </WelcomePageContainer>
+    </PageContainer>
+  );
 };
 
 export default WelcomePage;
---
+++ components/WelcomePage/styles.ts
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+export const WelcomePageContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  padding: 1rem 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    flex-direction: column-reverse;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    flex-direction: column;
+  }
+`;
+
+export const LeftPanel = styled.div`
+  display: flex;
+  align-items: flex-end;
+  height: 100%;
+  width: 100%;
+  padding-bottom: 4vw;
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    align-items: flex-start;
+  }
+`;
+
+export const TitleContainer = styled.h1`
+  & .myName {
+    white-space: nowrap;
+    font-size: 9.5rem;
+    line-height: 1;
+  }
+
+  & .myPosition {
+    color: ${(props) => props.theme.primary};
+    white-space: nowrap;
+    font-size: 12rem;
+    line-height: 1;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.fk}) {
+    & .myName {
+      font-size: 7rem;
+    }
+    & .myPosition {
+      font-size: 9rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tk}) {
+    & .myName {
+      font-size: 5.5rem;
+    }
+    & .myPosition {
+      font-size: 7.5rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.hd}) {
+    & .myName {
+      font-size: 4rem;
+    }
+    & .myPosition {
+      font-size: 6rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.xxl}) {
+    & .myPosition {
+      font-size: 5.5rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.xl}) {
+    & .myPosition {
+      font-size: 5rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    & .myPosition {
+      font-size: 4.5rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    & .myName {
+      font-size: 2.75rem;
+    }
+
+    & .myPosition {
+      font-size: 3.75rem;
+      white-space: normal;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    & .myPosition {
+      font-size: 2.875rem;
+    }
+  }
+`;
+
+export const RightPanel = styled.div`
+  height: 100%;
+  width: 100%;
+`;
