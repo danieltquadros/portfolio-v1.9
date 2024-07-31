c331556e47541f8e906a1c0da0628e714cb17c93
Daniel Quadros <80217777+danieltquadros@users.noreply.github.com>
2024-07-31 20:47:37 -0300

    [PORT-0015] - feat Iniciado a página About.

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -6,21 +6,137 @@ import PageContainer from '../PageContainer';
 
 // Styles
 import {
+  AboutCard,
   AboutContainer,
-  BackPanel,
-  ImageAndBack,
+  BackCard,
   ImagePanel,
-  LearnMoreButton,
+  ImageWindow,
+  InformationContainer,
+  Left,
   LeftPanel,
+  Right,
   RightPanel,
+  RightPanelMask,
+  SocialMediaCard,
+  SocialMediaContainer,
+  Strip,
+  StripPanelsPosition,
 } from './styles';
 import Image from 'next/image';
+import Button from '@/components/Button';
+import { mdiEmailOutline, mdiGithub, mdiLinkedin, mdiWhatsapp } from '@mdi/js';
+import Link from 'next/link';
+import Icon from '@mdi/react';
+
+interface SocialMedia {
+  id: string;
+  icon: string;
+  url: string;
+}
 
 const About = () => {
+  const stripPositionList: StripPanelsPosition[] = [
+    {
+      id: '1',
+      left: 20,
+      window: 60,
+      right: 20,
+    },
+    {
+      id: '2',
+      left: 10,
+      window: 80,
+      right: 10,
+    },
+    {
+      id: '3',
+      left: 0,
+      window: 100,
+      right: 0,
+    },
+    {
+      id: '4',
+      left: 10,
+      window: 80,
+      right: 10,
+    },
+    {
+      id: '5',
+      left: 20,
+      window: 60,
+      right: 20,
+    },
+  ];
+
+  const socialMediaList: SocialMedia[] = [
+    {
+      id: '1',
+      icon: mdiWhatsapp,
+      url: '',
+    },
+    {
+      id: '2',
+      icon: mdiLinkedin,
+      url: 'https://www.linkedin.com/in/elisiane-q/',
+    },
+    {
+      id: '3',
+      icon: mdiEmailOutline,
+      url: '',
+    },
+    {
+      id: '4',
+      icon: mdiGithub,
+      url: 'https://github.com/elisiane-quadros',
+    },
+  ];
+
   return (
     <PageContainer id="about">
       <AboutContainer>
         <LeftPanel>
+          {/* <BackCard /> */}
+          <AboutCard>
+            <InformationContainer>
+              <h2>Sobre mim</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+                facilis laudantium obcaecati aperiam. Iste deleniti, labore sed
+                quae asperiores, doloribus dolor esse praesentium suscipit non
+                aliquam totam inventore porro ipsam.
+              </p>
+            </InformationContainer>
+            <Button>Entre em contato</Button>
+          </AboutCard>
+        </LeftPanel>
+        <RightPanel>
+          <ImagePanel>
+            <RightPanelMask>
+              {stripPositionList.map((position) => {
+                return (
+                  <Strip key={position.id}>
+                    <Left position={position} />
+                    <ImageWindow position={position} />
+                    <Right position={position} />
+                  </Strip>
+                );
+              })}
+            </RightPanelMask>
+            <Image src="/perfil.png" width={631} height={659} alt="" />
+          </ImagePanel>
+          <SocialMediaContainer>
+            {socialMediaList.map((socialMedia) => {
+              return (
+                <SocialMediaCard key={socialMedia.id}>
+                  <Link href={socialMedia.url} target="_blank">
+                    <Icon path={socialMedia.icon} size={1.2} />
+                  </Link>
+                </SocialMediaCard>
+              );
+            })}
+          </SocialMediaContainer>
+        </RightPanel>
+        {/* <LeftPanel>
           <h2>Sobre</h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
@@ -39,7 +155,7 @@ const About = () => {
               <Image src="/perfil.png" height={100} width={100} alt="" />
             </ImagePanel>
           </ImageAndBack>
-        </RightPanel>
+        </RightPanel> */}
       </AboutContainer>
     </PageContainer>
   );
--- components/About/styles.ts
+++ components/About/styles.ts
@@ -1,153 +1,223 @@
 import styled from 'styled-components';
 
 export const AboutContainer = styled.div`
-  height: 100%;
   display: flex;
-  justify-content: center;
+  flex-direction: column-reverse;
+  justify-content: space-evenly;
   align-items: center;
+  position: relative;
+  gap: 1rem;
+  width: 100%;
+  min-height: calc(100vh - 90px);
+  padding: 1rem 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+  border-right: 1px solid ${(props) => props.theme.primary};
+  border-bottom: 1px solid ${(props) => props.theme.primary};
+  border-radius: 8px;
+
+  /* @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     flex-direction: column-reverse;
-  }
+  } */
+  //
 `;
 
 export const LeftPanel = styled.div`
-  width: 40vw;
-  height: 70vh;
-  border-radius: 4px 0 0 4px;
-  background-color: ${(props) => props.theme.lightness};
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  gap: 30px;
-  padding: 40px;
-
-  & h2 {
-    color: ${(props) => props.theme.darkOne};
-    font-size: 4rem;
-    text-align: start;
-  }
-
-  & p {
-    color: ${(props) => props.theme.darkOne};
-    max-width: 500px;
-    font-family: 'Inter', sans-serif;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
-    width: 100%;
-    border-radius: 0 0 4px 4px;
-
-    & p {
-      max-width: 700px;
-    }
-  }
 `;
 
-export const LearnMoreButton = styled.button`
-  border-width: 0;
-  width: fit-content;
-  padding: 0 2rem;
-  height: 40px;
-  background: ${(props) => props.theme.primary};
+export const BackCard = styled.div`
+  display: none;
+  width: calc(100vw - 2rem);
+  margin: 0 1rem;
+  max-width: 600px;
+  height: 300px;
+  border: 1px solid ${(props) => props.theme.primary};
+  border-radius: 8px;
+`;
 
-  border-radius: 4px;
-  cursor: pointer;
-  transition: 0.3s ease;
-  font-family: 'Oswald', sans-serif;
+export const AboutCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+  width: calc(100vw - 2rem);
+  margin: 0 1rem;
+  max-width: 600px;
+  /* background-image: linear-gradient(
+    to right bottom,
+    #242a33,
+    #1f2b2e,
+    #202a28,
+    #242823,
+    #272622
+  ); */
+  /* border-right: 1px solid ${(props) => props.theme.primary};
+  border-bottom: 1px solid ${(props) => props.theme.primary};
+  border-radius: 8px; */
+  padding: 1rem;
+`;
 
-  & span {
-    font-weight: 700;
+export const InformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  & p {
     text-align: center;
   }
 
-  &:hover {
-    background: #f5bf04; // definir cor do hover e adicionar no tema
+  & h2 {
+    font-size: 9.5rem;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.fk}) {
+    & h2 {
+      font-size: 7rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tk}) {
+    & h2 {
+      font-size: 5.5rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.hd}) {
+    & h2 {
+      font-size: 4rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    & h2 {
+      font-size: 3.25rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    & h2 {
+      font-size: 3.25rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    & h2 {
+      font-size: 2.75rem;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    & h2 {
+      font-size: 2.25rem;
+    }
   }
 `;
 
 export const RightPanel = styled.div`
-  width: 25vw;
-  height: 80vh;
-  border-radius: 4px;
-  background: ${(props) => props.theme.primary};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
-    width: 100%;
-    height: 12vh;
-    border-radius: 4px 4px 0 0;
-  }
-`;
-
-export const ImageAndBack = styled.div`
-  position: relative;
-  top: 160px;
-  right: 120px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
-    top: 20px;
-    right: 200px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
-    top: 80px;
-    right: -200px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
-    right: -100px;
-  }
-`;
-
-export const BackPanel = styled.div`
-  width: 24vw;
-  height: 24vw;
-  background-image: url('backImage.svg');
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
-    width: 30vw;
-    height: 30vw;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
-    width: 36vw;
-    height: 36vw;
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 `;
 
 export const ImagePanel = styled.div`
-  width: 24vw;
-  height: 24vw;
-  background: ${(props) => props.theme.darkTwo};
+  display: flex;
+  flex-direction: column;
   position: relative;
-  top: -27vw;
-  right: -3vw;
+  justify-content: flex-start;
+  align-items: center;
+  z-index: 1;
 
   & img {
-    width: 24vw;
-    height: 24vw;
-    box-shadow: -10px 10px 20px 10px ${(props) => props.theme.darkness};
-    object-fit: cover;
-    filter: grayscale(100%);
+    display: block;
+    position: relative;
+    width: calc(100vw - 32px);
+    height: auto;
+  }
+`;
+
+export const RightPanelMask = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  width: calc(100vw - 32px);
+  height: 100%;
+`;
+
+export type StripPanelsPosition = {
+  id: string;
+  left: number;
+  window: number;
+  right: number;
+};
+
+export interface StripPosition {
+  position: StripPanelsPosition;
+}
+
+export const Left = styled.div<StripPosition>`
+  background-color: ${(props) => props.theme.darkness};
+  width: ${(props) => props.position.left}%;
+  transition: 0.3s ease;
+`;
+
+export const ImageWindow = styled.div<StripPosition>`
+  width: ${(props) => props.position.window}%;
+  background: ${(props) => props.theme.primary}40;
+  border-top: 2px solid ${(props) => props.theme.darkness};
+  border-bottom: 2px solid ${(props) => props.theme.darkness};
+  transition: 0.3s ease;
+`;
+
+export const Right = styled.div<StripPosition>`
+  background-color: ${(props) => props.theme.darkness};
+  width: ${(props) => props.position.right}%;
+  transition: 0.3s ease;
+`;
+
+export const Strip = styled.div`
+  display: flex;
+  height: 20%;
+
+  &:hover ${Left} {
+    width: 0;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
-    width: 30vw;
-    height: 30vw;
-    top: -33vw;
-    & img {
-      width: 30vw;
-      height: 30vw;
-    }
+  &:hover ${ImageWindow} {
+    width: 100%;
+    background: transparent;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
-    width: 36vw;
-    height: 36vw;
-    top: -40vw;
-    & img {
-      width: 36vw;
-      height: 36vw;
-    }
+  &:hover ${Right} {
+    width: 0;
+  }
+`;
+
+export const SocialMediaContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  width: 100%;
+`;
+
+export const SocialMediaCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.theme.darkTwo};
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+
+  & a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
   }
 `;
