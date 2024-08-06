8c4c5980bab69629c79dde5bdd8fb92dd29ae49b
ElisianeQuadros <elisiane.quadros@hotmail.com>
2024-08-06 10:13:41 -0300

    [PORT-0016] - feat:commit para atualização da branch.

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
 // Global components
-import PageContainer from '../PageContainer';
+
+import Icon from '@mdi/react';
+import { mdiEmail, mdiGithub, mdiLinkedin, mdiWhatsapp } from '@mdi/js';
+import { ContactArea, ContactContainer } from './styles';
+import Link from 'next/link';
 
 const Contact = () => {
-  return <PageContainer id="contact">Contact</PageContainer>;
+  return (
+    <ContactContainer id="contact">
+      <ContactArea>
+        <Link href="#" target="_blank">
+          <Icon path={mdiEmail} size={1.5} />
+        </Link>
+        <Link href="https://github.com/elisiane-quadros" target="_blank">
+          <Icon path={mdiGithub} size={1.5} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/elisiane-q/" target="_blank">
+          <Icon path={mdiLinkedin} size={1.5} />
+        </Link>
+        <Link href="" target="_blank">
+          <Icon path={mdiWhatsapp} size={1.5} />
+        </Link>
+      </ContactArea>
+      <div>
+        <span>&copy2024-ElisianeQuadros</span>
+      </div>
+    </ContactContainer>
+  );
 };
 
 export default Contact;
--- components/Contact/styles.ts
+++ components/Contact/styles.ts
@@ -1,28 +1,42 @@
 import styled from 'styled-components';
 
 export const ContactContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
   width: 100vw;
-  height: 100vh;
-  padding: 90px 8vw 0 8vw;
-  background: ${(props) => props.theme.darkTwo};
+  height: 300px;
+  background-image: linear-gradient(
+    132deg,
+    ${(props) => props.theme.darkTwo} 0%,
+    ${(props) => props.theme.primary} 100%
+  );
+  box-shadow: 0px 4px 4px 1px ${(props) => props.theme.primary};
+`;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.fk}) {
-    padding: 120px 12vw 0 12vw;
+export const ContactArea = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 56px;
+  max-width: 700px;
+  transition: 0.3s ease;
+
+  & a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 56px;
+    height: 56px;
+    border: 1px solid ${(props) => props.theme.primary};
+    border-radius: 8px;
+    color: ${(props) => props.theme.primary};
+    cursor: pointer;
+    background: #272622;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.tk}) {
-    padding: 120px 10vw 0 10vw;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.hd}) {
-    padding: 90px 8vw 0 8vw;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
-    padding: 90px 4vw 0 4vw;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
-    padding: 0 4vw 90px 4vw;
+  & svg {
   }
 `;
